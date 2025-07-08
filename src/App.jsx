d09656eb868132f1244ffc0e9ebcb315f38a35e7
Ravi-khamla <ravikhamladeveloper@gmail.com>
2025-07-08 22:28:06 +0530

    Created hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import NavBar from './components/NavBar'
 import HeroSection from './sections/HeroSection'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
+import MessageSection from './sections/MessageSection'
+
+gsap.registerPlugin(ScrollTrigger);
 
 function App() {
   return (
     <main>
       <NavBar />
       <HeroSection />
+      <MessageSection />
     </main>
   )
 }
